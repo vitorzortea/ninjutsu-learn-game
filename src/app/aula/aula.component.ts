cf1e0ad5f5ddd2fad4d55ae462be8b34adeb08d9
Vitor Zortéa <vitorzortea@gmail.com>
2020-10-19 20:54:58 -0300

    lógica da aula

--- src/app/aula/aula.component.ts
+++ src/app/aula/aula.component.ts
@@ -22,27 +22,42 @@ export class AulaComponent implements OnInit {
     const videoAula = document.getElementById('video-aula') as HTMLVideoElement;
     videoAula.muted = true;
     videoAula.play();
+    this.setAnimationCSS();
+    setTimeout(() => { this.router.navigate(['/resposta']); }, 5000);
+  }
 
-    document.getElementById('corpo-espirito').animate(
-      [
-        {transform: 'rotate(0deg)'},
-        {transform: 'rotate(360deg)'},
-        {transform: 'rotate(' + (this.motor.grade[this.motor.atual].equilibrio + 360) + 'deg)'},
-      ],
-      { duration: 2000, iterations: 1, fill: 'forwards', }
-    );
-
-    const m = this.motor.grade[this.motor.atual].chakara;
-    document.querySelector('#painel span').animate(
-      [
-        {left: '24%'},
-        {left: '90%;'},
-        {left: (((m / 100) * 62) + 28) + '%'},
-      ],
-      { duration: 2000, iterations: 1, fill: 'forwards', }
-    );
-
-    setTimeout(() => { this.router.navigate(['/resposta']); }, 10000);
+  setAnimationCSS() {
+    const bodyFunction = document.querySelector('#animation-aula');
+    const chakara = (((this.motor.grade[this.motor.atual].chakara / 100) * 66) + 24);
+    const equilibrio = this.motor.grade[this.motor.atual].equilibrio + 360;
+    bodyFunction.innerHTML = `
+    <style>
+      #painel span{
+        animation: slide-chakara 3s forwards
+      }
+      #corpo-espirito{
+        animation: rotate-equilibrio 3s forwards
+      }
+      @keyframes slide-chakara{
+        0%{ left: 24%; }
+        40%{ left: 90%; }
+        60%{ left: ${chakara}%; }
+        70%{ left: ${chakara}%; transform: scale(1.2) }
+        80%{ left: ${chakara}%; transform: scale(1) }
+        90%{ left: ${chakara}%; transform: scale(1.2) }
+        100%{ left: ${chakara}%; }
+      }
+      @keyframes rotate-equilibrio{
+        0%{ transform: rotate(0deg); }
+        40%{ transform: rotate(360deg); }
+        60%{ transform: rotate(${equilibrio}deg); }
+        70%{ transform: rotate(${equilibrio}deg) scale(1.2); }
+        80%{ transform: rotate(${equilibrio}deg) scale(1); }
+        90%{ transform: rotate(${equilibrio}deg) scale(1.2); }
+        100%{ transform: rotate(${equilibrio}deg); }
+      }
+    </style>
+    `;
   }
 
 }
